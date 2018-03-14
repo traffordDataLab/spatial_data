e6957f133fcba9c41555ab7369cb58607ae19e64
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-03-14 14:57:29 +0000

    Added styling R script

---
+++ greenspaces/styling.R
@@ -0,0 +1,31 @@
+## Styling features ##
+
+# load packages ---------------------------
+library(sf) ; library(geojsonio) ; library(RColorBrewer) ; library(leaflet)
+
+# read data ---------------------------
+geojson <- st_read("https://github.com/traffordDataLab/spatial_data/raw/master/greenspaces/trafford_greenspace_sites.geojson")
+
+# apply styles ---------------------------
+geojson_styles <- geojson_style(geojson, var = 'site_type',
+                           stroke = brewer.pal(length(unique(geojson$site_type)), "Set2"),
+                           stroke_width = 3,
+                           stroke_opacity = 1,
+                           fill = brewer.pal(length(unique(geojson$site_type)), "Set2"),
+                           fill_opacity = 0.8)
+
+# check results ---------------------------
+leaflet() %>% 
+  addProviderTiles(providers$CartoDB.Positron) %>% 
+  setView(-2.35533522781156, 53.419025498197, zoom = 12) %>% 
+  addPolygons(data = geojson_styles, 
+              color = ~stroke, 
+              weight = ~stroke.width, 
+              opacity = ~stroke.opacity,
+              fillColor = ~fill,
+              fillOpacity = ~fill.opacity,
+              label = ~site_type)
+
+# write data ---------------------------
+st_write(geojson_styles, "trafford_greenspace_sites_styled.geojson")
+
