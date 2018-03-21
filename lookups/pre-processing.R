692a1c9360a8c7ca14903ac83cfa2186dc197748
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-03-21 11:24:41 +0000

    Updated lookups

--- lookups/pre-processing.R
+++ lookups/pre-processing.R
@@ -1,21 +1,23 @@
-## Pre-processing ##
+## Lookups ##
 
-library(tidyverse) ; library(sf)
+# Source: Open Goegraphy Portal 
+# Publisher URL: http://geoportal.statistics.gov.uk/
+# Licence: Open Government Licence 3.0
 
-# source: https://ons.maps.arcgis.com/home/item.html?id=baf6566ca08949c6bbd61ff81d9526da
-lsoa_to_local_authority <- read_csv("OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv") %>% 
+# load libraries---------------------------
+library(tidyverse)
+
+# https://ons.maps.arcgis.com/home/item.html?id=baf6566ca08949c6bbd61ff81d9526da
+read_csv("OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv") %>% 
   filter(LAD11NM %in% c("Bolton","Bury","Manchester","Oldham","Rochdale","Salford","Stockport","Tameside","Trafford","Wigan")) %>% 
-  select(lsoa_code = LSOA11CD, lsoa_name = LSOA11NM, la_code = LAD11CD, la_name = LAD11NM) %>% 
-  arrange(la_name)
-
-write_csv(lsoa_to_local_authority, "lsoa_to_local_authority.csv")
+  select(-LAD11NMW) %>% 
+  setNames(tolower(names(.))) %>% 
+  write_csv("statistical_lookup.csv")
 
 # source: http://geoportal.statistics.gov.uk/datasets/ward-to-local-authority-district-december-2017-lookup-in-the-united-kingdom
-ward_to_local_authority <- st_read("https://opendata.arcgis.com/datasets/046394602a6b415e9fe4039083ef300e_0.geojson") %>% 
-  filter(LAD17NM %in% c("Bolton","Bury","Manchester","Oldham","Rochdale","Salford","Stockport","Tameside","Trafford","Wigan")) %>%
-  select(ward_code = WD17CD, ward_name = WD17NM, la_code = LAD17CD, la_name = LAD17NM) %>% 
-  st_set_geometry(value = NULL) %>% 
-  arrange(la_name)
-
-write_csv(ward_to_local_authority, "ward_to_local_authority.csv")
+admiministrative <- read_csv("https://opendata.arcgis.com/datasets/046394602a6b415e9fe4039083ef300e_0.csv") %>% 
+  filter(LAD17NM %in% c("Bolton","Bury","Manchester","Oldham","Rochdale","Salford","Stockport","Tameside","Trafford","Wigan")) %>% 
+  select(-WD17NMW, -FID) %>% 
+  setNames(tolower(names(.))) %>%
+  write_csv("administrative_lookup.csv")
 
