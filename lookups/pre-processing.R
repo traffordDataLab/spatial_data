f192a6a35da622b71c7cdab6912c8c1aa97f30f8
rcatlord <henrypartridge@Henrys-MacBook-Pro.local>
2018-03-21 11:28:02 +0000

    Dropped column in administrative lookup

--- lookups/pre-processing.R
+++ lookups/pre-processing.R
@@ -17,7 +17,7 @@ read_csv("OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv") %>%
 # source: http://geoportal.statistics.gov.uk/datasets/ward-to-local-authority-district-december-2017-lookup-in-the-united-kingdom
 admiministrative <- read_csv("https://opendata.arcgis.com/datasets/046394602a6b415e9fe4039083ef300e_0.csv") %>% 
   filter(LAD17NM %in% c("Bolton","Bury","Manchester","Oldham","Rochdale","Salford","Stockport","Tameside","Trafford","Wigan")) %>% 
-  select(-WD17NMW, -FID) %>% 
+  select(-WD17NMW, -FID) %>%
   setNames(tolower(names(.))) %>%
   write_csv("administrative_lookup.csv")
 
