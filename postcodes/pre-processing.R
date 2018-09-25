39823d4e3cb790c73d2352c48787cf6b09f4defd
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-09-25 13:54:15 +0100

    Changed names to lon, lat

--- postcodes/pre-processing.R
+++ postcodes/pre-processing.R
@@ -11,10 +11,10 @@ library(tidyverse)
 read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>% 
   filter(oslaua %in% c("E08000001", "E08000002", "E08000003", "E08000004", "E08000005", "E08000006", 
     "E08000007", "E08000008", "E08000009", "E08000010")) %>% 
-  select(postcode = pcds, lat, long) %>% 
+  select(postcode = pcds, lon = long, lat) %>% 
   write_csv("GM_postcodes_2018-08.csv")
 
 read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>% 
   filter(oslaua == "E08000009") %>% 
-  select(postcode = pcds, lat, long) %>% 
+  select(postcode = pcds, lon = long, lat) %>% 
   write_csv("trafford_postcodes_2018-08.csv")
