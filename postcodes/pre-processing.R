8af577c6c5850c555176ff5c4bff396dfb8cbbab
rcatlord <henrypartridge@henrys-mbp.tht.local>
2018-09-25 13:29:07 +0100

    Updated ONS postcode directory - August 2018

--- postcodes/pre-processing.R
+++ postcodes/pre-processing.R
@@ -12,9 +12,9 @@ read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0e
   filter(oslaua %in% c("E08000001", "E08000002", "E08000003", "E08000004", "E08000005", "E08000006", 
     "E08000007", "E08000008", "E08000009", "E08000010")) %>% 
   select(postcode = pcds, lat, long) %>% 
-  write_csv("GM_postcodes_2018-05.csv")
+  write_csv("GM_postcodes_2018-08.csv")
 
 read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>% 
   filter(oslaua == "E08000009") %>% 
   select(postcode = pcds, lat, long) %>% 
-  write_csv("trafford_postcodes_2018-05.csv")
+  write_csv("trafford_postcodes_2018-08.csv")
