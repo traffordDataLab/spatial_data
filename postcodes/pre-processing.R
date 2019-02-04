aefb5eeda159db04fe5e97c83d5b58f76207bac9
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-02-04 17:23:15 +0000

    Updated postcode lookup

--- postcodes/pre-processing.R
+++ postcodes/pre-processing.R
@@ -5,16 +5,18 @@
 # Licence: Open Government Licence 3.0
 
 # load necessary packages ---------------------------
-library(tidyverse)
+library(tidyverse) ; library(sf)
 
-# read and write data ---------------------------
-read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>% 
-  filter(oslaua %in% c("E08000001", "E08000002", "E08000003", "E08000004", "E08000005", "E08000006", 
-    "E08000007", "E08000008", "E08000009", "E08000010")) %>% 
-  select(postcode = pcds, lon = long, lat) %>% 
-  write_csv("GM_postcodes_2018-08.csv")
+# read, tidy and write data ---------------------------
+sf <- st_read("https://www.trafforddatalab.io/spatial_data/ward/2017/trafford_ward_full_resolution.geojson") %>% 
+  select(area_code)
 
 read_csv("http://geoportal.statistics.gov.uk/datasets/75edec484c5d49bcadd4893c0ebca0ff_0.csv") %>% 
   filter(oslaua == "E08000009") %>% 
   select(postcode = pcds, lon = long, lat) %>% 
-  write_csv("trafford_postcodes_2018-08.csv")
+  st_as_sf(crs = 4326, coords = c("lon", "lat")) %>% 
+  mutate(lon = map_dbl(geometry, ~st_coordinates(.x)[[1]]),
+         lat = map_dbl(geometry, ~st_coordinates(.x)[[2]])) %>% 
+  st_join(sf, join = st_within, left = FALSE) %>% 
+  select(1:3, lon, lat) %>% 
+  write_csv("trafford_postcodes_2018-11.csv")
