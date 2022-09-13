5617334dd2f2ec5d1b398bbc591baaca6973bde9
James Austin <james.austin@trafford.gov.uk>
2022-09-13 15:42:02 +0100

    Added latest LSOA boundaries (2021). Currently only the full resolution version is available.

---
+++ lsoa/2021/pre-processing.R
@@ -0,0 +1,61 @@
+## Lower-layer Super Output Areas ##
+# 2022-09-13
+
+# load necessary packages
+library(sf) ; library(tidyverse)
+
+# Load source boundaries for whole country in all resolutions ---------
+
+# Full resolution
+# Source: https://geoportal.statistics.gov.uk/datasets/ons::lower-layer-super-output-areas-december-2021-boundaries-full-clipped-ew-bfc/explore?location=52.783351%2C-2.489483%2C7.00
+all_full <- st_read("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/LSOA_2021_EW_BFC_V2/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson") %>%
+  select(area_code = LSOA21CD, area_name = LSOA21NM) %>% 
+  st_as_sf(crs = 4326, coords = c("long", "lat"))
+
+# Generalised resolution
+# Source: Not available yet
+# all_gen <- st_read("") %>%
+#   select(area_code = LSOA21CD, area_name = LSOA21NM) %>% 
+#   st_as_sf(crs = 4326, coords = c("long", "lat"))
+
+# Super generalised resolution
+# Source: Not available yet
+# all_super_gen <- st_read("") %>%
+#   select(area_code = LSOA21CD, area_name = LSOA21NM) %>% 
+#   st_as_sf(crs = 4326, coords = c("long", "lat"))
+
+
+# Extract just Greater Manchester boundaries ---------
+
+# Full resolution
+gm_full <- all_full %>%
+  filter(grepl('Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan', area_name)) %>%
+  st_write("gm_lsoa_full_resolution.geojson", driver = "GeoJSON")
+
+# Generalised resolution
+gm_gen <- all_gen %>%
+  filter(grepl('Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan', area_name)) %>%
+  st_write("gm_lsoa_generalised.geojson", driver = "GeoJSON")
+
+# Super Generalised resolution
+gm_super_gen <- all_super_gen %>%
+  filter(grepl('Bolton|Bury|Manchester|Oldham|Rochdale|Salford|Stockport|Tameside|Trafford|Wigan', area_name)) %>%
+  st_write("gm_lsoa_super_generalised.geojson", driver = "GeoJSON")
+
+
+# Extract just Trafford boundaries ---------
+
+# Full resolution resolution
+traff_full <- all_full %>%
+  filter(grepl('Trafford', area_name)) %>%
+  st_write("trafford_lsoa_full_resolution.geojson", driver = "GeoJSON")
+
+# Generalised resolution
+traff_gen <- all_gen %>%
+  filter(grepl('Trafford', area_name)) %>%
+  st_write("trafford_lsoa_generalised.geojson", driver = "GeoJSON")
+
+# Super Generalised resolution
+traff_super_gen <- all_super_gen %>%
+  filter(grepl('Trafford', area_name)) %>%
+  st_write("trafford_lsoa_super_generalised.geojson", driver = "GeoJSON")
