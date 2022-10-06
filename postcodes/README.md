604b89d2f8a938922085b158d46dea455f6c5f0b
James Austin <james.austin@trafford.gov.uk>
2022-10-06 14:35:02 +0100

    Updated with the latest (August 2022) postcodes and also updated all associated boundaries e.g. wards, MSOAs, LSOAs etc. to the latest versions (2021 for statistical and 2022 for administrative).

--- postcodes/README.md
+++ postcodes/README.md
@@ -17,7 +17,7 @@
 </tr>
 <tr>
 	<td>Publisher URL</td>
-	<td><a href="https://geoportal.statistics.gov.uk/datasets/ons::ons-postcode-directory-may-2022/about">https://geoportal.statistics.gov.uk/datasets/ons::ons-postcode-directory-may-2022/about</a></td>
+	<td><a href="https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-august-2022/about">https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-august-2022/about</a></td>
 </tr>
 <tr>
 	<td>Geography</td>
@@ -29,11 +29,11 @@
 </tr>
 <tr>
 	<td>Temporal coverage</td>
-	<td>2022-05</td>
+	<td>2022-08</td>
 </tr>
 <tr>
 	<td>Update frequency</td>
-	<td>Not known</td>
+	<td>Quarterly</td>
 </tr>
 <tr>
 	<td>Licence</td>
@@ -53,7 +53,7 @@
 </tr>
 <tr>
 	<td>Last updated</td>
-	<td>2022-06-10</td>
+	<td>2022-10-06</td>
 </tr>
 <tr>
 	<td>Notes</td>
--- postcodes/pre-processing.R
+++ postcodes/pre-processing.R
@@ -1,45 +1,74 @@
-## ONS Postcode Directory (May 2022) ##
+## ONS Postcode Directory (August 2022) ##
 
 # Source: ONS Open Geography Portal
-# Publisher URL: https://geoportal.statistics.gov.uk/datasets/ons::ons-postcode-directory-may-2022/about
+# Publisher URL: https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-august-2022/about
 # Licence: Open Government Licence 3.0
 
-# load necessary packages ---------------------------
-library(tidyverse) ; library(sf) ; library(jsonlite)
+# NOTE: care must be taken to ensure that the boundary data, (wards, MSOAs, LSOAs) is kept up to date otherwise NAs will appear in the data against some postcodes
 
-# import and tidy data ---------------------------
+# load necessary packages ---------
+library(tidyverse) ; library(sf) ; library(httr)
 
-gm<- "Greater Manchester"
-lookup_ward_la_gm <- fromJSON(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/WD19_LAD19_CTY19_OTH_UK_LU/FeatureServer/0/query?where=",
-URLencode(paste0("CTY19NM = '", gm , "'"), reserved = TRUE),
-                       "&outFields=wd19cd,wd19nm,lad19cd,lad19nm&outSR=4326&f=json"), flatten = T) %>%
-  pluck("features") %>%
-  select(ward_code = attributes.WD19CD,
-         ward_name = attributes.WD19NM,
-         la_code = attributes.LAD19CD,
-         la_name = attributes.LAD19NM)
+
+# Wards ---------
+# Obtain lookup for Greater Manchester containing all the wards and their associated LAs
+# Source: https://geoportal.statistics.gov.uk/documents/ward-to-local-authority-district-may-2022-lookup-in-the-united-kingdom/about
+tmp <- tempfile(fileext = ".xlsx")
+GET(url = "https://www.arcgis.com/sharing/rest/content/items/27dee9a199cf4181b63fe2ea9b9c81f0/data",
+    write_disk(tmp))
+
+lookup_ward_la_gm <- read_xlsx(tmp) %>%
+  filter(LAD22NM %in% c("Bolton","Bury","Manchester","Oldham","Rochdale","Salford","Stockport","Tameside","Trafford","Wigan")) %>% 
+  select(ward_code = WD22CD,
+         ward_name = WD22NM,
+         la_code = LAD22CD,
+         la_name = LAD22NM)
+
+# delete the downloaded raw data
+unlink(tmp)
+
 
 # Using the MSOA name file from: https://houseofcommonslibrary.github.io/msoanames/
 # There is a static URL available with the latest data, however for reproducibility we use the specific version file
-msoa <- read_csv("https://houseofcommonslibrary.github.io/msoanames/MSOA-Names-1.16.csv") %>% 
-  filter(Laname %in% unique(lookup_ward_la_gm$la_name)) %>%
-  select(msoa_code=msoa11cd,msoa_hcl_name=msoa11hclnm)
+msoa <- read_csv("https://houseofcommonslibrary.github.io/msoanames/MSOA-Names-2.0.csv") %>% 
+  filter(localauthorityname %in% unique(lookup_ward_la_gm$la_name)) %>%
+  select(msoa_code=msoa21cd,msoa_hcl_name=msoa21hclnm)
 
-lsoa <- read_csv("https://www.trafforddatalab.io/spatial_data/lookups/statistical_lookup.csv") %>%
-  select(lsoa_code = lsoa11cd, lsoa_name = lsoa11nm) %>%
-  unique()
 
-url <- "https://www.arcgis.com/sharing/rest/content/items/39469ce1ae6441e7bc10ba0ba1ed5572/data"
-download.file(url, dest = "ONSPD_MAY_2022_UK.zip")
-unzip("ONSPD_MAY_2022_UK.zip", exdir = "ONSPD_MAY_2022_UK")
-file.remove("ONSPD_MAY_2022_UK.zip")
+# Statistical lookup OA -> LSOA -> MSOA -> LAD to get the LSOAs in each Greater
+# Source: https://geoportal.statistics.gov.uk/datasets/output-area-to-lower-layer-super-output-area-to-middle-layer-super-output-area-to-local-authority-district-december-2021-lookup-in-england-and-wales-v2/about
+tmp <- tempfile(fileext = ".csv")
+GET(url = "https://www.arcgis.com/sharing/rest/content/items/9f0bc2c6fbc9427ba11db01759e5f6d8/data",
+    write_disk(tmp))
 
-postcodes_gm <- read_csv("ONSPD_MAY_2022_UK/Data/ONSPD_MAY_2022_UK.csv") %>% 
+lsoa <- read_csv(tmp) %>%
+  filter(lad22nm %in% unique(lookup_ward_la_gm$la_name)) %>%
+  select(lsoa_code = lsoa21cd, lsoa_name = lsoa21nm) %>%
+  unique() # Need to remove duplicate LSOA entries (due to LSOAs containing multiple OAs but we've only extracted the LSOAs)
+
+# delete the downloaded raw data
+unlink(tmp)
+
+
+# Now download the actual postcodes file ---------
+pcode_file_reference <- "ONSPD_AUG_2022_UK" # makes it easier to change this once here than throughout the code below
+
+# https://geoportal.statistics.gov.uk/datasets/ons-postcode-directory-august-2022/about
+tmp <- tempfile(fileext = ".zip")
+GET(url = "https://www.arcgis.com/sharing/rest/content/items/8e0d123a946240288c3c84cf9f9cba28/data",
+    write_disk(tmp))
+
+unzip(tmp, exdir = pcode_file_reference) # extract the contents of the zip
+
+# delete the downloaded zip
+unlink(tmp)
+
+postcodes_gm <- read_csv(paste0(pcode_file_reference, "/Data/", pcode_file_reference, ".csv")) %>% 
   filter(oslaua %in% unique(lookup_ward_la_gm$la_code)) %>%
   select(postcode = pcds,
          ward_code = osward,
-         msoa_code = msoa11,
-         lsoa_code = lsoa11,
+         msoa_code = msoa21,
+         lsoa_code = lsoa21,
          la_code = oslaua,
          lon = long,
          lat = lat) %>% 
@@ -48,11 +77,7 @@ postcodes_gm <- read_csv("ONSPD_MAY_2022_UK/Data/ONSPD_MAY_2022_UK.csv") %>%
   left_join(lsoa, by = "lsoa_code") %>%
   select(postcode, ward_code, ward_name, msoa_code, msoa_hcl_name, lsoa_code, lsoa_name, la_code, la_name, lon, lat)
 
-# write data ---------------------------
-write_csv(postcodes_gm, "gm_postcodes.csv")
-
-# add localities info to Trafford ---------------------------
-
+# Filter for just postcodes in Trafford and add localities info
 trafford_postcodes <- postcodes_gm %>%
   filter(la_name=="Trafford") %>%
   mutate(locality = 
@@ -63,9 +88,10 @@ trafford_postcodes <- postcodes_gm %>%
              ward_name %in% c("Bucklow-St Martins", "Davyhulme East", "Davyhulme West", "Flixton", "Urmston") ~ "West")) %>%
   select(postcode, ward_code, ward_name, msoa_code, msoa_hcl_name, lsoa_code, lsoa_name, locality, la_code, la_name, lon, lat)
 
-
+# write data ---------
+write_csv(postcodes_gm, "gm_postcodes.csv")
 write_csv(trafford_postcodes, "trafford_postcodes.csv")
 
 
 # Tidy up filesystem ---------------------------
-unlink("ONSPD_MAY_2022_UK", recursive = TRUE)
+unlink(pcode_file_reference, recursive = TRUE)
